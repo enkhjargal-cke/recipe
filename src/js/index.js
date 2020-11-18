b9e1efd3d6558ab9289c29ab141003785818b085
enkhjargal <enkhee10300@gmail.com>
2020-11-18 20:47:32 +0800

    search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,32 @@
 import Search from "./model/Search";
 
-let search = new Search(`pasta`);
+/**
+    Web app tolow
+    -Хайлтын query, үр дүн
+    -Тухайн үзүүлж байгаа жор
+    -Лайкласан жорууд
+    -Захиалж байгаа жорын найрлагууд
+ */
 
-search.doSearch().then(r => console.log(r));
+const state = {};
+const  controllerSearch = async () => {
+    // 1. Вэбээс хайлтын түлхүүр үгийг гаргаж авна.
+    const query = 'pizza';
+    if (query) {
+        // 2. Шинээр хайлтын обьектыг үүсгэж өгнө.
+        state.search = new Search(query);
+        // 3. Хайлт хийхэд зориулж дэлгэцийг бэлтгэнэ.
+        // 4. Хайлыг гүйцэтгэнэ.
+        await state.search.doSearch();
+        // 5. Хайлын үр дүнг дэлгэцэнд үзүүлнэ.
+        console.log(state.search.result)
+    }
+
+    console.log('clicked');
+}
+
+document.querySelector('.search')
+    .addEventListener('submit', e => {
+        e.preventDefault();
+        controllerSearch();
+});
